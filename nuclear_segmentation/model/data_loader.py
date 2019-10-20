1e5da3a2a6c74204cb6ae84c182f19fb27b76fd5
Ilya Rudyak <ilyarudyak@yahoo.com>
2019-10-20 19:53:13 +0300

    added simple augmentation

--- nuclear_segmentation/model/data_loader.py
+++ nuclear_segmentation/model/data_loader.py
@@ -5,25 +5,50 @@ from torch.utils.data import Dataset, DataLoader
 import numpy as np
 from pathlib import Path
 from torchvision import transforms
-
+from torchvision.transforms import Compose
+import PIL
 
 data_dir = Path.home() / 'data/isbi2012/'
 args = Namespace(
     path_img_train=data_dir / 'train-volume.tif',
     path_target_train=data_dir / 'train-labels.tif',
     path_img_test=data_dir / 'test-volume.tif',
-    n_valid=5,
+    n_valid=3,  # we split our 30 examples between train and valid sets
     TRAIN='train',
     VAL='val',
     TEST='test'
 )
 
 
-def get_transforms():
+def get_transforms_initial():
     train_transforms = transforms.Compose([
         transforms.ToTensor()
     ])
-    val_transforms = transforms.Compose([
+    val_transforms: Compose = transforms.Compose([
+        transforms.ToTensor()
+    ])
+
+    return train_transforms, val_transforms
+
+
+def get_transforms_stanford():
+    """
+    We started from the example of a project:
+    http://cs230.stanford.edu/projects_winter_2019/reports/15810728.pdf
+    code for this project is not available, but we have code for a close project:
+
+    :return: train_transforms, val_transforms
+    """
+    train_transforms = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.RandomAffine(degrees=.2,
+                                translate=(.05, .05),
+                                shear=.05,
+                                resample=PIL.Image.NEAREST),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor()
+    ])
+    val_transforms: Compose = transforms.Compose([
         transforms.ToTensor()
     ])
 
@@ -73,6 +98,7 @@ class ISBI2012Dataset(Dataset):
             target = self.targets[idx]
 
         if self.transforms:
+            # it's necessary to apply the same transforms to target!
             img = self.transforms(img)
             target = self.transforms(target)
 
@@ -90,7 +116,7 @@ def fetch_dataloader(splits, params):
     Returns:
         dataloaders: (dict) contains the DataLoader object for each type in types
     """
-    train_transforms, val_transforms = get_transforms()
+    train_transforms, val_transforms = get_transforms_initial()
     dataloaders = {}
 
     for split in splits:
